3f74eeb89187038045e1636795a2e779896ecad9
heipiao233 <heipiao233@outlook.com>
2021-05-30 15:48:15 +0800

    1.16.5 support!

--- src/main/java/com/rakion99/usernamemod/UserNameMod.java
+++ src/main/java/com/rakion99/usernamemod/UserNameMod.java
@@ -1,35 +1,21 @@
 package com.rakion99.usernamemod;
 
-import net.minecraft.client.gui.GuiMainMenu;
-import net.minecraft.client.gui.GuiMultiplayer;
-import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.init.Blocks;
-import net.minecraftforge.client.event.GuiScreenEvent;
-import net.minecraftforge.client.event.GuiScreenEvent.InitGuiEvent;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.FMLCommonHandler;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-@Mod(modid = UserNameMod.MODID, version = UserNameMod.VERSION)
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(UserNameMod.MODID)
 public class UserNameMod
 {
     public static final String MODID = "usernamemod";
-    public static final String VERSION = "1.6";
+    public static final String VERSION = "1.7";
+    public static final Logger LOGGER = LogManager.getLogger();
     
-    @EventHandler
-    public void init(FMLInitializationEvent event)
+    public UserNameMod()
     {
-        System.out.println("<< UserNameMod 1.6 Loaded >> ");
-    }
-    
-    @EventHandler
-	public void preInit(FMLPreInitializationEvent event)  
-    {
-		
+        System.out.println("<< UserNameMod 1.7 Loaded >> ");
     	MinecraftForge.EVENT_BUS.register(new UserNameEvent());
 		
 	}
